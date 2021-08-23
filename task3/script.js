a7bd0239e1fa32466d2deacf6517287ebea21434
ja0112hk <ja0112hk@g,ail.com>
2021-08-24 01:44:27 +0900

    feat: create add li to ul

---
+++ task3/script.js
@@ -0,0 +1,15 @@
+const ul = document.getElementById('ul');
+
+for (let i =1; i <=2; i++) {
+    const li =document.createElement('li');
+    const a =document.createElement('a');
+    const img =document.createElement('img');
+
+    a.href = "a" + i + ".html";
+    a.textContent = "a" + i;
+
+    ul.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
+}
+
+const firstChild = ul.firstChild;
+
