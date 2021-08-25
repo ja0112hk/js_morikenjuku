06e5a33562c5715b42ccc7ed97eb13aab56db5af
ja0112hk <ja0112hk@g,ail.com>
2021-08-25 12:10:35 +0900

    refactor: Modifying loops and using arrays

--- task3/script.js
+++ task3/script.js
@@ -1,15 +1,17 @@
 const ul = document.getElementById('ul');
+const liLength = 2;
 
-for (let i =1; i <=2; i++) {
+for (let i =1; i <= liLength; i++) {
     const li =document.createElement('li');
     const a =document.createElement('a');
     const img =document.createElement('img');
+    const data = ["bookmark.png", "message.png"];
 
-    a.href = "a" + i + ".html";
-    a.textContent = "a" + i;
+    a.href = `a${i}.html`
+    a.textContent = `a${i}`;
+
+    img.src = `/img/${data[i - 1]}`;
+
 
     ul.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
 }
-
-ul.firstChild.querySelector('img').src = "/img/bookmark.png";
-ul.lastChild.querySelector('img').src = "/img/message.png";
