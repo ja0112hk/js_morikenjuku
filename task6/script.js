c80e2b5285509c2ed8962f163d96c39ea33b581a
ja0112hk <ja0112hk@g,ail.com>
2021-10-03 15:17:29 +0900

    feat: Implementation of a function that resolves after 3 seconds

---
+++ task6/script.js
@@ -0,0 +1,47 @@
+const df = document.createDocumentFragment();
+
+const ul = document.getElementById("ul");
+
+const listItem = [
+  {
+    to: "bookmark.html",
+    img: "1.png",
+    alt: "画像1",
+    text: "ブックマーク"
+  },
+  {
+    to: "message.html",
+    img: "2.png",
+    alt: "画像2",
+    text: "メッセージ"
+  }
+];
+
+const arrayPromise = () => {
+  return new Promise((resolve, reject) => {
+    resolve(listItem);
+  });
+};
+
+arrayPromise().then((value) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+      for (let i = 0; i < value.length; i++) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const img = document.createElement("img");
+
+        a.href = value[i].to;
+        a.textContent = value[i].text;
+
+        img.src = value[i].img;
+        img.alt = value[i].alt;
+
+        df.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin", img);
+      }
+      ul.appendChild(df);
+
+    }, 3000);
+  });
+});
